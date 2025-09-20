198458d9000e0a95f1cb5795937c4fe49855c939
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-21 01:14:02 +0200

    HashTable methods

--- src/main/java/thembelani/java/tutorials/HashTable/HashTable.java
+++ src/main/java/thembelani/java/tutorials/HashTable/HashTable.java
@@ -1,5 +1,6 @@
 package thembelani.java.tutorials.HashTable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class HashTable {
@@ -19,6 +20,72 @@ public class HashTable {
 
     public HashTable() {
         this.dataMap = new Node[this.size];
+    }
+
+    public ArrayList<String> keys() {
+        ArrayList<String> allKeys = new ArrayList<>();
+
+        for (int i = 0; i < dataMap.length; i++) {
+            Node temp = dataMap[i];
+
+            while (temp != null) {
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+
+        return allKeys;
+    }
+
+    public int get(String key) {
+        int index = hash(key);
+
+        if (dataMap[index] == null) {
+            return 0;
+        } else {
+            Node temp = dataMap[index];
+            while (temp != null) {
+                if (temp.key.equals(key)) {
+                    return temp.value;
+                }
+                temp = temp.next;
+            }
+        }
+        return 0;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            if (temp.key.equals(key)) {
+                temp.value = value;
+                return;
+            }
+            while (temp.next != null) {
+                temp = temp.next;
+                if (temp.key.equals(key)) {
+                    temp.value = value;
+                    return;
+                }
+            }
+            temp.next = newNode;
+        }
+
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
     }
 
     public void printTable() {
