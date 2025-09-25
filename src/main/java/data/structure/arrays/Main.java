b8ebf4cdb42bb75667f50b81091d07e7a955d685
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-25 22:47:31 +0200

    Arrays Binary Search

--- src/main/java/data/structure/arrays/Main.java
+++ src/main/java/data/structure/arrays/Main.java
@@ -15,35 +15,45 @@ public class Main {
         int[] secondArray = new int[10];
         System.out.println(Arrays.toString(secondArray));
 
-        Arrays.fill(secondArray,5);
+        Arrays.fill(secondArray, 5);
         System.out.println(Arrays.toString(secondArray));
 
         int[] thirdArray = getRandomArray(10);
         System.out.println(Arrays.toString(thirdArray));
 
-        int[] fourthArray = Arrays.copyOf(thirdArray,thirdArray.length);
+        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
         System.out.println(Arrays.toString(fourthArray));
 
         Arrays.sort(fourthArray);
         System.out.println(Arrays.toString(thirdArray));
         System.out.println(Arrays.toString(fourthArray));
 
-        String[] sArray = {"Able","Thembelani","Junior","Sinethemba","Mark","David"};
+        String[] sArray = {"Able", "Thembelani", "Junior", "Sinethemba", "Mark", "David"};
         Arrays.sort(sArray);
         System.out.println(Arrays.toString(sArray));
 
-        if(Arrays.binarySearch(sArray,"Thembelani") >= 0){
+        if (Arrays.binarySearch(sArray, "Thembelani") >= 0) {
             System.out.println("Found Thembelani in the list");
         }
 
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
+
+
     }
 
-    private static int[] getRandomArray(int len){
+    private static int[] getRandomArray(int len) {
 
         Random random = new Random();
         int[] newIntArray = new int[len];
 
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             newIntArray[i] = random.nextInt(100);
         }
 
