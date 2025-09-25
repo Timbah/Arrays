24e4b2551d9ed1aa16137739287f7f16a0bcb6d5
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-25 21:14:55 +0200

    Arrays sorting,filling, copying

--- src/main/java/data/structure/arrays/Main.java
+++ src/main/java/data/structure/arrays/Main.java
@@ -9,6 +9,25 @@ public class Main {
 
         int[] myIntArray = getRandomArray(10);
         System.out.println(Arrays.toString(myIntArray));
+        Arrays.sort(myIntArray);
+        System.out.println(Arrays.toString(myIntArray));
+
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+
+        Arrays.fill(secondArray,5);
+        System.out.println(Arrays.toString(secondArray));
+
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray = Arrays.copyOf(thirdArray,thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
 
     }
 
