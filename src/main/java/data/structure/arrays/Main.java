8a828c2baa10a73724e1270da77f29b8d08f7374
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-25 23:03:12 +0200

    Arrays reverse sort

--- src/main/java/data/structure/arrays/Main.java
+++ src/main/java/data/structure/arrays/Main.java
@@ -7,43 +7,58 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myIntArray = getRandomArray(10);
-        System.out.println(Arrays.toString(myIntArray));
-        Arrays.sort(myIntArray);
-        System.out.println(Arrays.toString(myIntArray));
+//        int[] myIntArray = getRandomArray(10);
+//        System.out.println(Arrays.toString(myIntArray));
+//        Arrays.sort(myIntArray);
+//        System.out.println(Arrays.toString(myIntArray));
+//
+//        int[] secondArray = new int[10];
+//        System.out.println(Arrays.toString(secondArray));
+//
+//        Arrays.fill(secondArray, 5);
+//        System.out.println(Arrays.toString(secondArray));
+//
+//        int[] thirdArray = getRandomArray(10);
+//        System.out.println(Arrays.toString(thirdArray));
+//
+//        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
+//        System.out.println(Arrays.toString(fourthArray));
+//
+//        Arrays.sort(fourthArray);
+//        System.out.println(Arrays.toString(thirdArray));
+//        System.out.println(Arrays.toString(fourthArray));
+//
+//        String[] sArray = {"Able", "Thembelani", "Junior", "Sinethemba", "Mark", "David"};
+//        Arrays.sort(sArray);
+//        System.out.println(Arrays.toString(sArray));
+//
+//        if (Arrays.binarySearch(sArray, "Thembelani") >= 0) {
+//            System.out.println("Found Thembelani in the list");
+//        }
+//
+//        int[] s1 = {1, 2, 3, 4, 5};
+//        int[] s2 = {1, 2, 3, 4, 5};
+//
+//        if (Arrays.equals(s1, s2)) {
+//            System.out.println("Arrays are equal");
+//        } else {
+//            System.out.println("Arrays are not equal");
+//        }
 
-        int[] secondArray = new int[10];
-        System.out.println(Arrays.toString(secondArray));
+        int[] arraySort = getRandomArray(5);
+        System.out.println(Arrays.toString(arraySort));
+        Arrays.sort(arraySort);
+        System.out.println(Arrays.toString(arraySort));
 
-        Arrays.fill(secondArray, 5);
-        System.out.println(Arrays.toString(secondArray));
+        int[] finalArray = new int[arraySort.length];
+        int temp = arraySort.length -1;
 
-        int[] thirdArray = getRandomArray(10);
-        System.out.println(Arrays.toString(thirdArray));
-
-        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
-        System.out.println(Arrays.toString(fourthArray));
-
-        Arrays.sort(fourthArray);
-        System.out.println(Arrays.toString(thirdArray));
-        System.out.println(Arrays.toString(fourthArray));
-
-        String[] sArray = {"Able", "Thembelani", "Junior", "Sinethemba", "Mark", "David"};
-        Arrays.sort(sArray);
-        System.out.println(Arrays.toString(sArray));
-
-        if (Arrays.binarySearch(sArray, "Thembelani") >= 0) {
-            System.out.println("Found Thembelani in the list");
+        for(int i =0; i < finalArray.length; i++){
+            finalArray[i] = arraySort[temp];
+            temp--;
         }
 
-        int[] s1 = {1, 2, 3, 4, 5};
-        int[] s2 = {1, 2, 3, 4, 5};
-
-        if (Arrays.equals(s1, s2)) {
-            System.out.println("Arrays are equal");
-        } else {
-            System.out.println("Arrays are not equal");
-        }
+        System.out.println(Arrays.toString(finalArray));
 
 
     }
