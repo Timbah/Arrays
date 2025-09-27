b926a8573064cfd9e4d38eeae5e573641385f0dd
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-27 10:57:28 +0200

    Minimum Element in an array challenge

---
+++ src/main/java/Minimum/Element/Challenge/MinimumElement.java
@@ -0,0 +1,32 @@
+package Minimum.Element.Challenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumElement {
+
+    public int[] readIntegers() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a comma separated list of integers e.g. 1,3,89,15");
+
+        String input = scanner.nextLine(); // Read the full line
+        String[] stringNumbers = input.split(","); // Split into string array
+
+        int[] intArray = new int[stringNumbers.length]; // Create int array
+
+        for (int i = 0; i < stringNumbers.length; i++) {
+            intArray[i] = Integer.parseInt(stringNumbers[i].trim()); // Convert and trim
+        }
+
+        return intArray;
+    }
+
+    public int getMin(int[] arrayInput) {
+
+        Arrays.sort(arrayInput);
+
+        return arrayInput[0];
+    }
+
+}
