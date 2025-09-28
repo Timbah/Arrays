97b7180a62fb3dff4a44760af5eda2e144bd89a8
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-28 22:15:57 +0200

    Find minimum from user input

--- src/main/java/Minimum/Element/Challenge/MinimumElement.java
+++ src/main/java/Minimum/Element/Challenge/MinimumElement.java
@@ -25,11 +25,31 @@ public class MinimumElement {
 
         int min = arrayInput[0];
 
-        for(int element : arrayInput){
+        for (int element : arrayInput) {
             min = element < min ? element : min;
         }
 
         return min;
     }
 
+    public static int readInteger() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of elements to be entered: ");
+        return scanner.nextInt();
+
+    }
+
+    public static int[] readElements(int numOfElements) {
+
+        int[] elementsArray = new int[numOfElements];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numOfElements; i++) {
+            System.out.println("Enter the next element: ");
+            elementsArray[i] = scanner.nextInt();
+        }
+
+        return elementsArray;
+    }
 }
