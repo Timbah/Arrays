3b7779e540df8652e2b6ffb4b85b5136cb21e4d7
Thembelani Ngema <thembelaningema20@gmail.com>
2025-09-30 19:57:05 +0200

    Multidimensional arrays

---
+++ src/main/java/MultipleArrays/Main.java
@@ -0,0 +1,13 @@
+package MultipleArrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] array2 = new int[4][4];
+
+        System.out.println(Arrays.deepToString(array2));
+    }
+}
